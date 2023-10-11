6bd3abdc57107e0d964e205ccce1f55c4320a05a
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-10-11 16:58:08 +0300

    Add project files.

---
+++ AP4.3/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+
+	while (x <= xk)
+	{
+		if (c < 0 && x != 0)
+			F = (-a * x) - c;
+		else
+			if (c > 0 && x == 0)
+				F = (x - a) / (-c);
+			else
+				F = (b * x) / (c - a);
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |" << setw(10) << setprecision(3) << F << " |" << endl;
+
+		x += dx;
+	}
+
+	cout << "---------------------------" << endl;
+
+	return 0;
+}
